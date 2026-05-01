bf8c71d9bbc84bf6c71b3320c9beccb431ae9a88
JM Digitalz <jmdigitalzofficial1242@gmail.com>
2026-05-01 14:42:02 +0500

    Fix import.meta error for CommonJS

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,11 +3,10 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
-import { fileURLToPath } from "url";
+
 import router from "./routes";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+
 
 const app: any = express();
 
